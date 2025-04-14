05ac194d07ef1d676790a99ba0cb0f5e5c876eeb
pappu-jsr <pappu985214@gmail.com>
2025-04-14 16:43:12 +0530

    )01_variable updated

---
+++ 01_variable.js
@@ -0,0 +1,9 @@
+const accountId=11455
+let accountEmail="priyansh@gamil.com"
+var accountPassword="12345"
+accountCity="ranchi"
+// accountId=1234567 // not allowed
+let accountState="current state"
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountState])
