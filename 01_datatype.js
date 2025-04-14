2d010eff79c2448443e4eeaa6474ab259067bc86
pappu-jsr <pappu985214@gmail.com>
2025-04-14 19:08:18 +0530

    datatype updated

---
+++ 01_datatype.js
@@ -0,0 +1,28 @@
+"using strict";//treat all js code as new version 
+//alert(3+3) //we are using node.js, not browser
+
+console.log(3
+    +
+    3
+);
+
+
+let name="priyansh"
+let age=21
+let isloggedIn=false
+let state;
+
+//number => 2 to power 53
+//string => ""
+//boolean => true/false
+//null => standing alone
+//undefine 
+// symbol => unique
+ 
+//object
+
+console.log(typeof name);//string
+console.log(typeof age);// number
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
--- 01_variable.js
+++ 01_variable.js
@@ -4,6 +4,10 @@ var accountPassword="12345"
 accountCity="ranchi"
 // accountId=1234567 // not allowed
 let accountState="current state"
+/*
+prefer not to use var 
+because of issue in block scope and function scope
+*/
 
 console.log(accountId)
 console.table([accountId,accountEmail,accountPassword,accountState])
