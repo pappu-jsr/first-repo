060981fc2cf3f3f3331cbfc65fbb143fe481d83a
pappu-jsr <pappu985214@gmail.com>
2025-04-15 22:37:00 +0530

    stack&heap added

---
+++ stack&heap_memory.js
@@ -0,0 +1,21 @@
+// stack(primitive datatypes), heap(none-primitive datatype)
+
+let myYotubeName="priyansh gameing"
+let anotherName=myYotubeName
+anotherName="Coding With Ansh"
+
+console.log(myYotubeName)
+console.log(anotherName);
+
+let userone={
+    Name:"priyansh",
+    upi:"priyansh@yble",
+}
+
+let userTwo=userone
+userTwo.Name="ansh"
+
+console.log(userone.Name);
+console.log(userTwo.Name);
+
+
